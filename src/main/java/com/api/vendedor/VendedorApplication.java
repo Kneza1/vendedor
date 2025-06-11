3316ae9f122c98472a5c6aa9c66c35e8bb7d2a45
benjamin castro <lilbenjalemos@gmail.com>
2025-06-10 21:03:53 -0400

    finalizado

--- src/main/java/com/api/vendedor/VendedorApplication.java
+++ src/main/java/com/api/vendedor/VendedorApplication.java
@@ -3,7 +3,7 @@ package com.api.vendedor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.api.vendedor")
 public class VendedorApplication {
 
 	public static void main(String[] args) {
